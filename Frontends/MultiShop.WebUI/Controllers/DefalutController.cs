bd90606b9e2fe69251ac87d8d8a11df5c2cffda9
90536 <90536@DESKTOP-FCHRJ42>
2024-08-09 14:24:29 +0300

    UILayout

---
+++ Frontends/MultiShop.WebUI/Controllers/DefalutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.Controllers
+{
+    public class DefalutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
